8379c490ae3aa0d2c7d32a617a030e587d125b91
Alexander <awwakum@gmail.com>
2016-10-08 14:16:46 +0200

    day13

--- src/Day13/MyBook.java
+++ src/Day13/MyBook.java
@@ -1,6 +1,18 @@
 package Day13;
 
-public class MyBook
-{
-   
+class MyBook extends Book {
+
+    private int price;
+
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
 }
