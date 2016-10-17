e85baeaa7354c81062db971e4273bf82c492c3f2
acvetkov <alexander.cvetkov@md.de>
2016-10-17 13:53:28 +0200

    Day21

---
+++ src/Day21/Solution.java
@@ -0,0 +1,22 @@
+package Day21;
+
+import java.util.Arrays;
+
+public class Solution {
+
+	private static <T> void printArray(T[] arr) {
+		Arrays.stream(arr).forEach(System.out::println);
+	}
+
+	public static void main(String args[]) {
+		Integer[] intArray = { 1, 2, 3 };
+		String[] stringArray = { "Hello", "World" };
+
+		printArray(intArray);
+		printArray(stringArray);
+
+		if (Solution.class.getDeclaredMethods().length > 2) {
+			System.out.println("You should only have 1 method named printArray.");
+		}
+	}
+}
